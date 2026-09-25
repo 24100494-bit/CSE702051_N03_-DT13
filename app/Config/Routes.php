fa201659ad9a5843a135a8615d723d20c6d87b71
Cham Duong Ngoc <chamduongcs@gmail.com>
2026-09-25 16:32:03 +0700

    feat(v3): implement object authorization for de tai

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->group('api/v1', function ($routes) {
+    $routes->get('health', 'HealthController::index');
+
+    $routes->get('de-tai', 'DeTaiController::index');
+    $routes->get('de-tai/(:num)', 'DeTaiController::show/$1');
+}
+);
